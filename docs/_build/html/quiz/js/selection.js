5f9508b7986e60522540ab7c36d7c7fb31e4d448
Romnick Mamisay <rmamisay@integritynet.biz>
2019-03-25 10:46:17 +0800

    Minifed CSS and Javascript codes

--- docs/_build/html/quiz/js/selection.js
+++ docs/_build/html/quiz/js/selection.js
@@ -1,15 +1,1 @@
-$('.selection-group').unbind("click");
-$('.selection-group').on("click",function(e){
-    e.stopPropagation();
-    var target = e.target;
-    var parent_target = $(target).closest('.selection').find('input');
-    if (parent_target && parent_target.length > 0){
-        for (let index = 0; index < parent_target.length; index++) {
-            $(parent_target[index]).removeAttr('checked');
-        }
-    }
-    if ($(this).find('input').is(':enabled')){
-        $(this).find('input').prop("checked", true);
-        $(this).find('input').attr("checked", 'checked');    
-    }    
-});
+$('.selection-group').unbind("click");$('.selection-group').on("click",function(e){e.stopPropagation(); var target=e.target; var parent_target=$(target).closest('.selection').find('input'); if (parent_target && parent_target.length > 0){for (let index=0; index < parent_target.length; index++){$(parent_target[index]).removeAttr('checked');}}if ($(this).find('input').is(':enabled')){$(this).find('input').prop("checked", true); $(this).find('input').attr("checked", 'checked');}});
