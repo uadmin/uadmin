4d0c44255f7b1d56f43647dca27b6ea7a5a5d1f3
Abdullah Alrasheed <abdullah@integritynet.biz>
2019-08-20 13:55:14 +0800

    Add Mutex Lock for Rate Limit and update Font Awesome

--- rate_limit.go
+++ rate_limit.go
@@ -3,14 +3,17 @@ package uadmin
 import (
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
 var rateLimitMap = map[string]int64{}
+var rateLimitLock = sync.Mutex{}
 
 // CheckRateLimit checks if the request has remaining quota or not. If it returns false,
 // the IP in the request has exceeded their quota
 func CheckRateLimit(r *http.Request) bool {
+	rateLimitLock.Lock()
 	ip := r.RemoteAddr
 	index := strings.LastIndex(ip, ":")
 	now := time.Now().Unix() * RateLimit
@@ -24,6 +27,7 @@ func CheckRateLimit(r *http.Request) bool {
 	}
 
 	rateLimitMap[ip]++
+	rateLimitLock.Unlock()
 	if rateLimitMap[ip] > now {
 		return false
 	}
