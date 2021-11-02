169e86d59b0af3b110fdd10a9e117e7e00a739e5
Abdullah Alrasheed <twistedhardware@gmail.com>
2021-11-02 19:19:23 +0400

    Add CreatedAt and ModifiedAt and their tags

--- process_form.go
+++ process_form.go
@@ -62,6 +62,7 @@ func processForm(modelName string, w http.ResponseWriter, r *http.Request, sessi
 		}
 	}
 
+	// Check for UpdatedBy
 	_, hasUpdatedBy := t.FieldByName("UpdatedBy")
 	_, hasUpdatedByID := t.FieldByName("UpdatedByID")
 	hasCustomUpdatedByID := false
@@ -74,6 +75,31 @@ func processForm(modelName string, w http.ResponseWriter, r *http.Request, sessi
 		}
 	}
 
+	// Check for CreatedAt
+	_, hasCreatedAt := t.FieldByName("CreatedAt")
+	hasCustomCreatedAt := false
+	customCreatedAt := ""
+	for index := 0; index < t.NumField(); index++ {
+		if strings.Contains(t.Field(index).Tag.Get("uadmin"), "updated_at") {
+			hasCustomCreatedAt = true
+			customCreatedAt = t.Field(index).Name
+			break
+		}
+	}
+
+	// Check for ModifiedAt
+	_, hasModifiedAt := t.FieldByName("ModifiedAt")
+	hasCustomModifiedAt := false
+	customModifiedAt := ""
+	for index := 0; index < t.NumField(); index++ {
+		if strings.Contains(t.Field(index).Tag.Get("uadmin"), "modified_at") {
+			hasCustomModifiedAt = true
+			customModifiedAt = t.Field(index).Name
+			break
+		}
+	}
+
+	// Check for validation method
 	_, isValidate := t.MethodByName("Validate")
 
 	// Process Fields
@@ -375,6 +401,22 @@ func processForm(modelName string, w http.ResponseWriter, r *http.Request, sessi
 				m.Elem().FieldByName(customUpdatedByID).SetUint(uint64(user.ID))
 			}
 		}
+		if hasCreatedAt {
+			if m.Elem().FieldByName("CreatedAt").Type() == DType {
+				m.Elem().FieldByName("CreatedAt").Set(reflect.ValueOf(now))
+			}
+			if m.Elem().FieldByName("CreatedAt").Type() == DType1 {
+				m.Elem().FieldByName("CreatedAt").Set(reflect.ValueOf(&now))
+			}
+		}
+		if hasCustomCreatedAt {
+			if m.Elem().FieldByName(customCreatedAt).Type() == DType {
+				m.Elem().FieldByName(customCreatedAt).Set(reflect.ValueOf(now))
+			}
+			if m.Elem().FieldByName(customCreatedAt).Type() == DType1 {
+				m.Elem().FieldByName(customCreatedAt).Set(reflect.ValueOf(&now))
+			}
+		}
 	} else {
 		if hasCreatedBy {
 			if m.Elem().FieldByName("CreatedBy").Type().Kind() == reflect.String {
@@ -389,6 +431,22 @@ func processForm(modelName string, w http.ResponseWriter, r *http.Request, sessi
 		if hasCustomCreatedByID {
 			if m.Elem().FieldByName(customCreatedByID).Type().Kind() == reflect.Uint {
 				m.Elem().FieldByName(customCreatedByID).SetUint(uint64(user.ID))
+			}
+		}
+		if hasModifiedAt {
+			if m.Elem().FieldByName("ModifiedAt").Type() == DType {
+				m.Elem().FieldByName("ModifiedAt").Set(reflect.ValueOf(now))
+			}
+			if m.Elem().FieldByName("ModifiedAt").Type() == DType1 {
+				m.Elem().FieldByName("ModifiedAt").Set(reflect.ValueOf(&now))
+			}
+		}
+		if hasCustomModifiedAt {
+			if m.Elem().FieldByName(customModifiedAt).Type() == DType {
+				m.Elem().FieldByName(customModifiedAt).Set(reflect.ValueOf(now))
+			}
+			if m.Elem().FieldByName(customModifiedAt).Type() == DType1 {
+				m.Elem().FieldByName(customModifiedAt).Set(reflect.ValueOf(&now))
 			}
 		}
 	}
