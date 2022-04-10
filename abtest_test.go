f366deb0a8e6dd04495c327bd1ea30247fd4f7b7
Abdullah Alrasheed <twistedhardware@gmail.com>
2022-04-10 21:28:52 +0400

    Add unit tests for AB testing model data

--- abtest_test.go
+++ abtest_test.go
@@ -1,6 +1,7 @@
 package uadmin
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -16,6 +17,8 @@ func TestABTest(t *testing.T) {
 	}
 	test01.Save()
 
+	test01.Save()
+
 	if test01.ID == 0 {
 		t.Errorf("ABTest was not saved correctly. Got 0 ID")
 	}
@@ -23,21 +26,57 @@ func TestABTest(t *testing.T) {
 		t.Errorf("abTestCount didn't increment correctly. Expected %d but got %d", currentAbTestsCount+1, abTestCount)
 	}
 
+	test02 := ABTest{
+		Name:      "test 02",
+		Type:      ABTestType(0).Model(),
+		ModelName: ModelList(0),
+		Field:     FieldList(0),
+		Active:    true,
+		Group:     "test 1",
+	}
+	test02.Save()
+
+	if test02.ID == 0 {
+		t.Errorf("ABTest was not saved correctly. Got 0 ID")
+	}
+	if abTestCount != currentAbTestsCount+2 {
+		t.Errorf("abTestCount didn't increment correctly. Expected %d but got %d", currentAbTestsCount+1, abTestCount)
+	}
+
 	// Add Values
-	valueA := ABTestValue{
+	value1A := ABTestValue{
 		ABTestID: test01.ID,
 		Value:    "A",
 		Active:   true,
 	}
-	valueB := ABTestValue{
+	value1B := ABTestValue{
 		ABTestID: test01.ID,
 		Value:    "B",
 		Active:   true,
 	}
-	Save(&valueA)
-	Save(&valueB)
+	Save(&value1A)
+	Save(&value1B)
+	value2A := ABTestValue{
+		ABTestID: test02.ID,
+		Value:    "A",
+		Active:   true,
+	}
+	value2B := ABTestValue{
+		ABTestID: test02.ID,
+		Value:    "B",
+		Active:   true,
+	}
+	Save(&value2A)
+	Save(&value2B)
 	syncABTests()
 	r := &http.Request{}
+	r.Header = http.Header{}
+	abt := getABT(r)
+	r.AddCookie(&http.Cookie{
+		Name:  "abt",
+		Value: fmt.Sprint(abt),
+		Path:  "/",
+	})
 	ABTestClick(r, "test 1")
 	time.Sleep(time.Millisecond * 10)
 	// We lock and unlock to ensure that ABTestClick is done
@@ -47,20 +86,47 @@ func TestABTest(t *testing.T) {
 	syncABTests()
 
 	// get abt
-	abt := getABT(r)
 
 	// Check click
 	clicksRight := 0
 	clicksWrong := 0
 	if abt%2 == 0 {
-		GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", valueA.ID)
-		GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", valueB.ID)
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", value1A.ID)
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", value1B.ID)
 	} else {
-		GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", valueA.ID)
-		GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", valueB.ID)
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", value1A.ID)
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", value1B.ID)
 	}
 	if clicksRight != 1 {
-		t.Errorf("Expected 1 click for the right value got %d", clicksRight)
+		t.Errorf("Test 1 Expected 1 click for the right value got %d", clicksRight)
+	}
+	if clicksWrong != 0 {
+		t.Errorf("Test 1 Expected 0 click for the wrong value got %d", clicksWrong)
+	}
+	clicksRight = 0
+	clicksWrong = 0
+	if abt%2 == 0 {
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", value2A.ID)
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", value2B.ID)
+	} else {
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", value2A.ID)
+		GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", value2B.ID)
+	}
+	if clicksRight != 1 {
+		t.Errorf("Test 2 Expected 1 click for the right value got %d", clicksRight)
+	}
+	if clicksWrong != 0 {
+		t.Errorf("Test 2 Expected 0 click for the wrong value got %d", clicksWrong)
+	}
+
+	// test reset
+	test01.Reset()
+	clicksRight = 0
+	clicksWrong = 0
+	GetValueSorted("ab_test_values", "clicks", "", true, &clicksRight, "id = ?", value1A.ID)
+	GetValueSorted("ab_test_values", "clicks", "", true, &clicksWrong, "id = ?", value1B.ID)
+	if clicksRight != 0 {
+		t.Errorf("Expected 0 click for the right value got %d", clicksRight)
 	}
 	if clicksWrong != 0 {
 		t.Errorf("Expected 0 click for the wrong value got %d", clicksWrong)
