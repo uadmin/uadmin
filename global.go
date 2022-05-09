ae5520b47302a22fd26f3e39803995ec5a846b86
Abdullah Alrasheed <twistedhardware@gmail.com>
2022-05-09 11:25:58 +0400

    0.8.0

--- global.go
+++ global.go
@@ -80,14 +80,15 @@ const cEMAIL = "email"
 const cM2M = "m2m"
 
 // Version number as per Semantic Versioning 2.0.0 (semver.org)
-const Version = "0.7.3"
+const Version = "0.8.8"
 
 // VersionCodeName is the cool name we give to versions with significant changes.
 // This name should always be a bug's name starting from A-Z them revolving back.
 // This started at version 0.5.0 (Atlas Moth)
 // 0.6.0 Beetle
 // 0.7.0 Catterpiller
-const VersionCodeName = "Catterpiller"
+// 0.8.0 Dragonfly
+const VersionCodeName = "Dragonfly"
 
 // Public Global Variables
 
