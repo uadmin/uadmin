f91e8923cfed99d30ad898d974b6c77122a4a204
Abdullah Alrasheed <a@maman.ae>
2022-11-12 10:38:39 +0400

    Add support for sending email attachments

--- send_email.go
+++ send_email.go
@@ -1,15 +1,19 @@
 package uadmin
 
 import (
+	"encoding/base64"
 	"fmt"
+	"mime"
 	"net/smtp"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 	"unicode/utf8"
 )
 
 // SendEmail sends email using system configured variables
-func SendEmail(to, cc, bcc []string, subject, body string) (err error) {
+func SendEmail(to, cc, bcc []string, subject, body string, attachments ...string) (err error) {
 	if EmailFrom == "" || EmailUsername == "" || EmailPassword == "" || EmailSMTPServer == "" || EmailSMTPServerPort == 0 {
 		errMsg := "Email not sent because email global variables are not set"
 		Trail(WARNING, errMsg)
@@ -30,6 +34,31 @@ func SendEmail(to, cc, bcc []string, subject, body string) (err error) {
 
 	// Construct the email
 	MIME := "MIME-version: 1.0;\r\nContent-Type: text/html; charset=\"utf-8\";\r\nContent-Transfer-Encoding: quoted-printable\r\n"
+	delimeter := fmt.Sprintf("--=_uAdmin_%s_%s.%s", GenerateBase32(3), GenerateBase32(9), GenerateBase32(10))
+	if len(attachments) != 0 {
+		MIME = "MIME-version: 1.0;\r\n"
+		MIME += fmt.Sprintf("Content-Type: multipart/mixed; boundary=\"%s\"\r\nContent-Transfer-Encoding: quoted-printable\r\n", delimeter)
+		body = "\r\n--" + delimeter + "\r\n" + fmt.Sprintf("Content-Type: text/html; boundary=\"%s\"\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\n", delimeter) + body
+
+		for i := range attachments {
+			// get filename
+			filename := filepath.Base(attachments[i])
+
+			// read the file
+			rawFile, err := os.ReadFile(attachments[i])
+			if err != nil {
+				Trail(WARNING, "Unable to attach file %s. %s", attachments[i], err)
+				continue
+			}
+
+			body += "\r\n--" + delimeter + "\r\n"
+			body += fmt.Sprintf("Content-Type: "+getMimeFromFileName(filename)+"; boundary=\"%s\"\r\n", delimeter)
+			body += "Content-Transfer-Encoding: base64\r\n"
+			body += "Content-Disposition: attachment;filename=\"" + filename + "\"\r\n"
+			body += "Content-Transfer-Encoding: quoted-printable\r\n"
+			body += "\r\n" + strings.Join(splitString(base64.StdEncoding.EncodeToString(rawFile), 73), "")
+		}
+	}
 
 	msg := "From: " + EmailFrom + "\r\n"
 	msg += "To: " + strings.Join(to, ",") + "\r\n"
@@ -75,4 +104,13 @@ func splitString(v string, maxLen int) []string {
 	}
 	splits = append(splits, v[l:])
 	return splits
+}
+
+func getMimeFromFileName(v string) string {
+	ext := filepath.Ext(v)
+	mType := mime.TypeByExtension(ext)
+	if mType == "" {
+		return "application/octet-stream"
+	}
+	return mType
 }
