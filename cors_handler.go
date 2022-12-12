b49258f27489d18f3d7f134ab2c2c7825d7eb5ab
Abdullah Alrasheed <a@maman.ae>
2022-12-12 09:20:21 +0400

    support multiple domains in CORS

--- cors_handler.go
+++ cors_handler.go
@@ -16,20 +16,20 @@ func CORSHandler(f func(w http.ResponseWriter, r *http.Request)) func(w http.Res
 				w.Header().Add("Access-Control-Allow-Origin", "*")
 			}
 		} else {
-			// allowedOrigin := false
-			// for i := range AllowedCORSOrigins {
-			// 	if strings.EqualFold(r.Header.Get("Origin"), AllowedCORSOrigins[i]) {
-			// 		allowedOrigin = true
-			// 		break
-			// 	}
-			// }
-			// if allowedOrigin {
-			// 	w.Header().Add("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-			// 	w.Header().Add("Access-Control-Allow-Credentials", "true")
-			// } else {
-			w.Header().Add("Access-Control-Allow-Origin", strings.Join(AllowedCORSOrigins, "|"))
-			w.Header().Add("Access-Control-Allow-Credentials", "true")
-			// }
+			allowedOrigin := false
+			for i := range AllowedCORSOrigins {
+				if strings.EqualFold(r.Header.Get("Origin"), AllowedCORSOrigins[i]) {
+					allowedOrigin = true
+					break
+				}
+			}
+			if allowedOrigin {
+				w.Header().Add("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+				w.Header().Add("Access-Control-Allow-Credentials", "true")
+			} else {
+				w.Header().Add("Access-Control-Allow-Origin", strings.Join(AllowedCORSOrigins, "|"))
+				w.Header().Add("Access-Control-Allow-Credentials", "true")
+			}
 
 		}
 
